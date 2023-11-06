541f85111d3a3210d5b965dada5be4c3b4cbce67
Mauricio Pallares <mauriciopallares10@gmail.com>
2023-11-06 00:18:13 -0500

    cambios en Formulario.jsx

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -78,7 +78,7 @@ const Formulario = () => {
         <ol className="list-group">
           {
               lista.map((item, index) => 
-                  (<li key={index} className="list-group-item list-group-item-warning">{item.nombre} {item.apellido}
+                  (<li key={index} className="list-group-item list-group-item-warning">ID: {index+1} <br /> Nombre: {item.nombre} <br /> Apellido: {item.apellido}
                       <button className="btn btn-danger float-end" onClick={() => eliminarUsuario(index)}>Eliminar</button>
                       <button className="btn btn-primary float-end me-2" onClick={() => editarUsuario(index)}>Editar</button>
                   </li>))
